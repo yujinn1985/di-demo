1245150809261dbde89b59f0601591fc2ae592f6
Eugene <myyujinn@gmail.com>
2018-01-28 14:42:00 +0200

    Java based configuration and beans factory added

---
+++ src/main/java/com/gmail/myyujinn/config/GreetingServiceConfig.java
@@ -0,0 +1,40 @@
+package com.gmail.myyujinn.config;
+
+import com.gmail.services.GreetingRepository;
+import com.gmail.services.GreetingService;
+import com.gmail.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository) {
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory) {
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+}
